eba632e8c761395a35086551a72346fa29b65487
Hector Palomares <hector.palomares@laureate.net>
2017-11-17 18:04:28 -0600

    Recomment ObserverList. Add Subject class

--- Design Patterns/observer-pattern.js
+++ Design Patterns/observer-pattern.js
@@ -1,9 +1,10 @@
 //	Observer Pattern: Object (subject) mantains a list of objects depending on it (observers), automatically notifying them any changes of state
+
+// List of dependent Observers
 function ObserverList() {
 	this.observerList = [];
 }
 
-//	Add an Observer
 ObserverList.prototype.add = function(obj) {
 	return this.ObserverList.push(obj);
 };
@@ -34,7 +35,29 @@ ObserverList.prototype.indexOf = function(obj, startIndex){
   return -1;
 };
 
-//	Remove an observer from the list
+
 ObserverList.prototype.remove = function(index) {
 	this.ObserverList.splice(index, 1);
+};
+
+// Subject with the ability to add, remove or notify observers on the observer list
+function Subject() {
+	this.observers = new ObserverList();
+};
+
+//	Add an Observer
+Subject.prototype.addObserver = function(observer) {
+	this.observers.add(observer);
+};
+
+//	Remove an observer from the list
+Subject.prototype.removeObserver = function(observer) {
+	this.observers.removeAt(this.observers.indexOf(observer, 0));
+};
+
+Subject.prototype.notify - function(context) {
+	let observerCount = this.observers.count();
+	for(let i = 0; i < i observerCount; i++) {
+		this.observers.get(i).update(context);
+	}
 };
