c7e288fbd9db6338eed48c729f1b24156785385f
Hector Palomares <hector.palomares@laureate.net>
2017-12-08 18:13:50 -0600

    Mixin Pattern: Part 2, Mixins

--- Design Patterns/Creational Design Patterns/mixin-pattern.js
+++ Design Patterns/Creational Design Patterns/mixin-pattern.js
@@ -30,3 +30,87 @@ console.log(superman); // Person { firstName: 'Clark', lastName: 'Kent', gender:
 // Superhero constructor creates an object which descends from Person. Have the attributes, methods and default values et in Person. 
 // Superhero can override any inherted values.
 
+/*Mixins*/
+
+// Simple Car Constructor
+let Car = function(settings) {
+	this.model = settings.model || "no model provided";
+	this.color = settings.color || "no color provided";
+	this.year = settings.year || "no year provided";
+};
+
+let Spaceship = function(settings) {
+	this.builder = settings.builder || "no builder provided";
+	this.tripulants = settings.tripulants || "no tripulants provided";
+	this.cost = settings.cost || "no cost provided";
+};
+
+// Mixin with to inhert from
+let Mixin = function() {};
+
+Mixin.prototype = {
+	driveForward: function() {
+		console.log("Drive Forward");
+	},
+	driveBackward: function() {
+		console.log("Drive Backward");
+	},
+	driveSideways: function() {
+		console.log("Drive Sideways");
+	},
+	liftToSpace: function() {
+		console.log("Lifting to Space!");
+	}
+};
+
+function augment(receivingClass, givingClass) {
+	// only provide certain methods
+    if(arguments[2]) {
+        for(let i = 2, len = arguments.length; i < len; i++) {
+        	receivingClass.prototype[arguments[i]] = givingClass.prototype[arguments[i]];
+        }
+        // provide all methods
+    } else {
+        for(let methodName in givingClass.prototype) {
+ 
+            // check to make sure the receiving class doesn't have a method of the same name as the one currently being proccesed by the for loop
+            if ( !Object.hasOwnProperty.call(receivingClass.prototype, methodName) ) {
+            	receivingClass.prototype[methodName] = givingClass.prototype[methodName];
+            }
+
+            // Alternatively (check prototype chain as well):
+            if ( !receivingClass.prototype[methodName] ) {
+            	receivingClass.prototype[methodName] = givingClass.prototype[methodName];
+            }
+        }
+    }
+}
+
+// Augment the Car constructor to include "driveForward" and "driveBackward" only
+augment(Car, Mixin, "driveForward", "driveBackward");
+
+//	Augment the Spaceship constructor to include all methods from our mixin
+augment(Spaceship, Mixin);
+
+// Create a new Car
+let myOldCar = new Car({model: "Sonic", color: "grey"});
+
+myOldCar.driveForward();	//	Drive Forward
+myOldCar.driveBackward();	// 	Drive Backward
+// myOldCar.driveSideways();	//	driveSideways not a function Undefined method, not included (line 90)
+
+console.log(myOldCar);
+
+// Create a new Spaceship
+let mySpaceShip = new Spaceship({builder: "Nasa", tripulants: 4, cost: "500000"});
+
+mySpaceShip.driveForward();		//	Drive Forward
+mySpaceShip.driveSideways();	// 	Drive Sideways
+mySpaceShip.liftToSpace();		// 	Lifting to space
+
+console.log(mySpaceShip);
+
+/*Advantages*/
+// Decrease functional repetition and increasing function re-use in a system. Avoid duplication by mantaining shared functionality. 
+/*Disadvantages*/
+// Prototype pollution, uncerainty regarding the origin of our functions (large systems). Strong documentation can minimize this confusion.
