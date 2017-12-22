23de80c23fe2b0cd99becf571996093a14cb1a45
Hector Palomares <hector.palomares@laureate.net>
2017-12-22 12:39:41 -0600

    Singleton. Printer Example.

--- Design Patterns/Creational Design Patterns/singleton-pattern.js
+++ Design Patterns/Creational Design Patterns/singleton-pattern.js
@@ -1,4 +1,6 @@
 //	Singleton Pattern: Restricts instantiation of a class to a single object.
+
+// Examples: Singleton templates, a GoodSingleton and a BadSingleton.
 let mySingleton = (function () {
 	//	Instance stores a reference to the Singleton
 	let instance;
@@ -9,6 +11,7 @@ let mySingleton = (function () {
 		function privateMethod() {
 			console.log("I am a private method");
 		}
+
 		let privateVariable = "Im also a priavte variable";
 		let privateRandomNumber = Math.random();
 
@@ -17,10 +20,13 @@ let mySingleton = (function () {
 			publicMethod: function() {
 				console.log("The public can see me!");
 			},
+			
 			publicProperty: "I am also public",
+			
 			getRandomNumber: function() {
 				return privateRandomNumber;
 			}
+
 		};
 	}
 
@@ -72,8 +78,9 @@ let badSingleB = myBadSingleton.getInstance();
 
 console.log(badSingleA.getRandomNumber() === badSingleB.getRandomNumber()); //	false, they are different instances!
 
-// Core Concept of the Singleton is the global access to the instance (through mySingleton.getInstance())
+// Example #2: Singleton with options
 
+// Core Concept of the Singleton is the global access to the instance (through mySingleton.getInstance())
 let SingletonTester = (function () {
 	
 	// options: an object containing configuration options for the singleton
@@ -104,8 +111,39 @@ let SingletonTester = (function () {
 let singletonTest1 = SingletonTester.getInstance({pointX: 5, pointY: 3});
 let singletonTest2 = SingletonTester.getInstance({pointX: 1, pointY: 7});
  
-console.log( singletonTest1.pointX, singletonTest.pointY );	// 5 3, instance set to 5 3
-console.log( singletonTest2.pointX, singletonTest2.pointY );	// 5 3, instance arleady set by SingletonTest1 to 5 3
+console.log(singletonTest1.pointX, singletonTest1.pointY);	// 5 3, instance set to 5 3
+console.log(singletonTest2.pointX, singletonTest2.pointY);	// 5 3, instance already set by SingletonTest1 to 5 3
+
+// Example #3: Singleton to a printer connection. Only allows for a single instantiation, but many instances of the same object.
+// Step 1 > Declare a single instance (line 124)
+// Step 2 > Declare constructor, methods and variables needed (line 126+)
+// Step 3 > Check if there is some instance already declare to return, if not create a new instance and return it (line 133+)
+
+let PrinterSingleton = (function () {
+	
+	let printerInstance;
+
+	function Printer(options = {}) {
+		this.name = options.name;
+		this.model = options.model;
+		this.place = options.place;
+	}
+
+	return {
+		getInstance: function(options) {
+			if(printerInstance === undefined) {
+				printerInstance = new Printer(options);
+			}
+			return printerInstance;
+		}
+	};
+
+})();
+
+let printer1 = PrinterSingleton.getInstance({name: "Printer-Coffee-Shop-1", model: "HP1670", place: "Coffee-Shop-Winterfell"});
+let printer2 = PrinterSingleton.getInstance({name: "Printer-Coffee-Shop-2", model: "HP9900", place: "Office-Kingslanding"});
+
+console.log(printer1 === printer2);	// true, printer2 is the same instance as printer1
 
 /*A&D*/
 // Advantages: Modules will use this function exactly the same way. Force to be onlu onw instance of this class.
